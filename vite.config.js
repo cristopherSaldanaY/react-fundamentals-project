1988db5d891db0d50f23614ed32c407d3daff92d
cristophersaldanay <cristopher.saldanay@gmail.com>
2023-08-29 09:52:09 -0400

    codigo final

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import imageminPlugin from "vite-plugin-imagemin";
 
 const env = dotenv.config();
 
-
-
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    imageminPlugin({ optipng: {}, pngquant: {}, jpegtran: {} }),
+  ],
+  server: {
+    port: 3001,
+  },
+  preview: {
+    port: 4270,
+  },
+  build: {
+    incremental: true,
+    babel: {
+      presets: ["@babel/preset-env", "@babel/present-react"],
+    },
+    cache: true,
+    minify: true,
+    cssMinify: true,
+    mode: "production",
+    chunks: true,
+    moduleBuilding: true,
+    debug: true,
+    prerenderPaths: ["/"],
+    modulePreload: true,
+    outDir: "build",
+  },
+  assetsInclude: ["assets"],
+});
